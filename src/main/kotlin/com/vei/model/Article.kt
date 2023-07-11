d3c83acab43e62343f16f2495aa27cd28b80b5b3
Tomas Krason <tomas.krason@vacuumlabs.com>
2023-07-11 09:08:29 +0200

    Remove unused services

--- src/main/kotlin/com/vei/model/Article.kt
+++
@@ -1,6 +0,0 @@
-package com.vei.model
-
-data class Article(
-    val articleName: String,
-    val articleText: String,
-)
--- src/main/kotlin/com/vei/services/UserService.kt
+++
@@ -1,8 +0,0 @@
-package com.vei.services
-
-import com.vei.model.User
-import com.vei.repository.UserRepository
-import org.koin.core.annotation.Singleton
-
-@Singleton
-class UserService(userRepository: UserRepository) : ModelService<User>(userRepository)
